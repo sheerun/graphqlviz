50421516b1bcf39fefd97706a8c1504878aca68a
Adam Stankiewicz <sheerun@sher.pl>
2016-03-25 19:10:15 +0100

    Use introspection query from graphql package

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 // https://github.com/NathanRSmith/graphql-visualizer
 
 var _ = require('lodash');
+var graphql = require('graphql');
+
+module.exports.query = graphql.introspectionQuery;
+console.log(module.exports.query);
 
 // process a graphql type object
 // returns simplified version of the type
@@ -155,5 +159,3 @@ module.exports.render = function (schema) {
 
   return dotfile;
 };
-
-module.exports.query = 'query IntrospectionQuery { __schema { queryType { name } mutationType { name } subscriptionType { name } types { ...FullType } directives { name description args { ...InputValue } onOperation onFragment onField } } } fragment FullType on __Type { kind name description fields(includeDeprecated: true) { name description args { ...InputValue } type { ...TypeRef } isDeprecated deprecationReason } inputFields { ...InputValue } interfaces { ...TypeRef } enumValues(includeDeprecated: true) { name description isDeprecated deprecationReason } possibleTypes { ...TypeRef } } fragment InputValue on __InputValue { name description type { ...TypeRef } defaultValue } fragment TypeRef on __Type { kind name ofType { kind name ofType { kind name ofType { kind name } } } }';
